5c874750d7602f4c8ec97addef8199ebfe7b62b9
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-10-04 22:26:03 -0400

    Add fetch logic to get data once user reaches end of page. Add infinite-scroll-component and it's rendering children.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import './App.css'
 
-function App() {
+export default function App () {
+  const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(0)
+
+  async function fetchData () {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=10`
+    )
+    const data = await response.json()
+    console.log(data)
+    setPhotos([...photos, ...data])
+    setPage(page + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <h1>List</h1>
+
+      <button onClick={fetchData}>Fetch photos</button>
+
+      <div className='container'>
+        <InfiniteScroll
+          dataLength={photos.length}
+          next={fetchData}
+          hasMore={true}
+          loader={<p>Loading...</p>}
+          endMessage={<p>End of list</p>}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            {photos &&
+              photos.map((item, index) => {
+                return <ListItem item={item} key={index} />
+              })}
+          </div>
+        </InfiniteScroll>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+function ListItem (props) {
+  return (
+    <div className='photo' id={props.item.id}>
+      <h6>{props.item.title}</h6>
+      <img src={`${props.item.url}`} alt='random' />
+    </div>
+  )
+}
